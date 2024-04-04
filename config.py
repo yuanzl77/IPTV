05fd9fc30304cab6ec390495d62f3ee8fd23275d
圆l <158799657+yuanzl77@users.noreply.github.com>
2024-04-04 21:27:46 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 class Config:
-     
-source_urls = [
-     "https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt",
-     "https://raw.githubusercontent.com/Guovin/TV/master/result.txt"
-  ]
+    source_urls = [
+        "https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt",
+        "https://raw.githubusercontent.com/Guovin/TV/master/result.txt"
+    ]
