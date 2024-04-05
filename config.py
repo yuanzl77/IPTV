8284735f5d0cfd086ce9e40d470e1e9aabe30db3
圆l <158799657+yuanzl77@users.noreply.github.com>
2024-04-05 12:37:01 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 class Config:
     source_urls = [
+        "https://xhdwc.tk/xiutan.m3u",
         "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/央视频道.txt",
         "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/嗅探.txt",
         "https://raw.githubusercontent.com/yuanzl77/TV/master/user_result.txt",
