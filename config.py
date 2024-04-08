4dfa88f4f2b0ff34c88cb97ba455a4a56a627858
圆l <158799657+yuanzl77@users.noreply.github.com>
2024-04-09 06:23:29 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
     source_urls = [
-        "http://www.liuyunw.com:88/zb.txt",
+        "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/央视频道.txt",
         "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/嗅探.txt",
         "https://raw.githubusercontent.com/yuanzl77/TV/master/user_result.txt",
         "https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt"
