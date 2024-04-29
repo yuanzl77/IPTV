62b141c4e0e4df52daa6a0f6fcf0a202aa7f6a2f
圆l <158799657+yuanzl77@users.noreply.github.com>
2024-04-30 06:12:45 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,6 @@ class Config:
         "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/央视频道.txt",
         "http://139.159.198.36:777/hwycs.txt",
         "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/wspd.txt",
-        "https://raw.githubusercontent.com/Guovin/TV/master/result.txt",
-        "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/综合直播频道.m3u",
-        "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/嗅探.txt",
+        "https://raw.githubusercontent.com/Guovin/TV/gd/result.txt",
         "https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt"
     ]
