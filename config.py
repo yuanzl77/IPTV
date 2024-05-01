6853769d42505a1505bdb0083f91888ad4ea610d
圆l <158799657+yuanzl77@users.noreply.github.com>
2024-05-01 08:55:57 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-class Config:
-    source_urls = [
-        "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/cctv.txt",
-        "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/央视频道.txt",
-        "http://139.159.198.36:777/hwycs.txt",
-        "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/wspd.txt",
-        "https://raw.githubusercontent.com/Guovin/TV/gd/result.txt",
-        "https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt"
-    ]
+ipv_type = "ipv4"
+source_urls = [
+    "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/cctv.txt",
+    "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/央视频道.txt",
+    "http://139.159.198.36:777/hwycs.txt",
+    "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/wspd.txt",
+    "https://raw.githubusercontent.com/Guovin/TV/gd/result.txt",
+    "https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt"
+]
