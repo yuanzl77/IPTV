639e8421564872d86476599ae1ba87ab3238cf73
圆l <158799657+yuanzl77@users.noreply.github.com>
2024-05-08 20:25:28 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ source_urls = [
     "http://175.178.251.183:6689/cctv2.txt",
     "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/wspd.txt",
     "https://raw.githubusercontent.com/Guovin/TV/gd/result.txt",
-    "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/ipv6.txt",
     "https://raw.githubusercontent.com/PizazzGY/TVBox_warehouse/main/live.txt",
+    "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/ipv6.txt",
     "https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt"
 ]
