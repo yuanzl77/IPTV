2b64521da2ab88ffa10efda300f07bcbfe89fc76
圆l <158799657+yuanzl77@users.noreply.github.com>
2024-05-12 08:29:09 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,9 @@ source_urls = [
     "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/cctv.txt",
     "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/央视频道.txt",
     "http://175.178.251.183:6689/cctv2.txt",
-    "http://router.bopop.buzz:9999/new/mdlive.txt",
-    "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/wspd.txt",
     "https://raw.githubusercontent.com/Guovin/TV/gd/result.txt",
     "https://raw.githubusercontent.com/PizazzGY/TVBox_warehouse/main/live.txt",
     "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/ipv6.txt",
+    "http://router.bopop.buzz:9999/new/mdlive.txt",
     "https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt"
 ]
