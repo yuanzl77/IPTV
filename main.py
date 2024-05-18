f5d2170dfa5e2eba4d04daaf87d061b38edf95c0
圆l <158799657+yuanzl77@users.noreply.github.com>
2024-05-18 08:04:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def parse_template(template_file):
     template_channels = OrderedDict()
     current_category = None
 
-    with open(template_file, "r") as f:
+    with open(template_file, "r", encoding="utf-8") as f:
         for line in f:
             line = line.strip()
             if line and not line.startswith("#"):
@@ -74,24 +74,27 @@ def updateChannelUrlsM3U(channels, template_channels):
     """
     written_urls = set()  # Set to store written URLs
 
-    with open("live.m3u", "w") as f:
-        f.write("#EXTM3U\n")
-        f.write("""#EXTINF:-1 tvg-id="1" tvg-name="请阅读" tvg-logo="https://gitee.com/yuanzl77/TVBox-logo/raw/main/mmexport1713580051470.png" group-title="公告",请阅读\n""")
-        f.write("https://liuliuliu.tv/api/channels/1997/stream\n")
-        f.write("""#EXTINF:-1 tvg-id="1" tvg-name="yuanzl77.github.io" tvg-logo="https://gitee.com/yuanzl77/TVBox-logo/raw/main/mmexport1713580051470.png" group-title="公告",yuanzl77.github.io\n""")
-        f.write("https://liuliuliu.tv/api/channels/233/stream\n")
+    with open("live.m3u", "w", encoding="utf-8") as f_m3u:
+        f_m3u.write("#EXTM3U\n")
+        f_m3u.write("""#EXTINF:-1 tvg-id="1" tvg-name="请阅读" tvg-logo="https://gitee.com/yuanzl77/TVBox-logo/raw/main/mmexport1713580051470.png" group-title="公告",请阅读\n""")
+        f_m3u.write("https://liuliuliu.tv/api/channels/1997/stream\n")
+        f_m3u.write("""#EXTINF:-1 tvg-id="1" tvg-name="yuanzl77.github.io" tvg-logo="https://gitee.com/yuanzl77/TVBox-logo/raw/main/mmexport1713580051470.png" group-title="公告",yuanzl77.github.io\n""")
+        f_m3u.write("https://liuliuliu.tv/api/channels/233/stream\n")
 
-        for category, channel_list in template_channels.items():
-            if category in channels:
-                for channel_name in channel_list:
-                    if channel_name in channels[category]:
-                        for url in channels[category][channel_name]:
-                            if url and url not in written_urls:  # Check if URL is not already written
-                                f.write(f"#EXTINF:-1 tvg-id=\"\" tvg-name=\"{channel_name}\" tvg-logo=\"https://gitee.com/yuanzl77/TVBox-logo/raw/main/png/{channel_name}.png\" group-title=\"{category}\",{channel_name}\n")
-                                f.write(url + "\n")
-                                written_urls.add(url)  # Add URL to written URLs set
+        with open("live.txt", "w", encoding="utf-8") as f_txt:
+            for category, channel_list in template_channels.items():
+                f_txt.write(f"{category},#genre#\n")
+                if category in channels:
+                    for channel_name in channel_list:
+                        if channel_name in channels[category]:
+                            for url in channels[category][channel_name]:
+                                if url and url not in written_urls:  # Check if URL is not already written
+                                    f_m3u.write(f"#EXTINF:-1 tvg-id=\"\" tvg-name=\"{channel_name}\" tvg-logo=\"https://gitee.com/yuanzl77/TVBox-logo/raw/main/png/{channel_name}.png\" group-title=\"{category}\",{channel_name}\n")
+                                    f_m3u.write(url + "\n")
+                                    f_txt.write(f"{channel_name},{url}\n")
+                                    written_urls.add(url)  # Add URL to written URLs set
 
-        f.write("\n")
+            f_txt.write("\n")
 
 if __name__ == "__main__":
     template_file = "demo.txt"
