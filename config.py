b374e7bb9cff07c0ab172da48ca95df5bbfc457f
圆l <158799657+yuanzl77@users.noreply.github.com>
2024-06-02 13:07:04 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 source_urls = [
     "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/央视频道.txt",
-    "http://175.178.251.183:6689/cctv2.txt",
+    "http://120.79.4.185/new/mdlive.txt",
+    "https://raw.githubusercontent.com/Fairy8o/IPTV/main/DIYP-v4.txt",
+    "http://rihou.cc:88/hccx/zb.txt",
+    "http://rihou.cc:555/gggg.nzk",
+    "https://tv.youdu.fan:666/live/",
+    "https://fs-im-kefu.7moor-fs1.com/ly/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/1715581924111/live1.txt",
     "https://raw.githubusercontent.com/Guovin/TV/gd/result.txt",
     "https://raw.githubusercontent.com/PizazzGY/TVBox_warehouse/main/live.txt",
     "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/ipv6.txt",
