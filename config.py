25d83f79ac82b2a44969dfef3b61d849a167ce38
圆l <158799657+yuanzl77@users.noreply.github.com>
2024-06-10 12:41:28 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ source_urls = [
     "https://raw.githubusercontent.com/Fairy8o/IPTV/main/DIYP-v4.txt",
     "https://tv.youdu.fan:666/live/",
     "http://175.178.251.183:6689/livv.txt",
+    "http://175.178.251.183:6689/livv2.txt",
     "https://fs-im-kefu.7moor-fs1.com/ly/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/1715581924111/live1.txt",
     "http://rihou.cc:88/hccx/zb.txt",
     "https://raw.githubusercontent.com/Guovin/TV/gd/result.txt",
