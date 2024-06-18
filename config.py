77faf2b3fac08febe7435fe297b0996e7934e271
圆l <158799657+yuanzl77@users.noreply.github.com>
2024-06-18 16:43:41 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,21 @@
+ip_version_priority = "ipv6"
 source_urls = [
+    "https://live.fanmingming.com/tv/m3u/ipv6.m3u"
     "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/央视频道.txt",
     "http://120.79.4.185/new/mdlive.txt",
     "https://raw.githubusercontent.com/Fairy8o/IPTV/main/DIYP-v4.txt",
     "https://tv.youdu.fan:666/live/",
     "http://175.178.251.183:6689/livv.txt",
     "https://fm1077.serv00.net/litv.txt",
-    "https://fanmingming.com/txt?url=http://175.178.251.183:6689/livv2.m3u",
     "https://raw.githubusercontent.com/mlvjfchen/TV/main/iptv_list.txt",
     "https://fs-im-kefu.7moor-fs1.com/ly/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/1715581924111/live1.txt",
     "http://rihou.cc:88/hccx/zb.txt",
     "https://live.goodiptv.club/api/getlive.php",
     "https://raw.githubusercontent.com/PizazzGY/TVBox_warehouse/main/live.txt",
-    "https://fanmingming.com/txt?url=https://raw.githubusercontent.com/hiyoi/IPTV/main/IPTV.m3u",
+    "https://raw.githubusercontent.com/hiyoi/IPTV/main/IPTV.m3u",
     "https://raw.githubusercontent.com/cymz6/AutoIPTV-Hotel/main/lives.txt",
     "https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt"
 ]
-
 url_blacklist = [
     "epg.pw/stream/",
     "103.40.13.71:12390/mytv/live.php"
