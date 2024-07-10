7d3d8c3f68fe91e9febb3ac673fbd4bb580ddc89
圆l <158799657+yuanzl77@users.noreply.github.com>
2024-07-10 11:14:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -132,7 +132,7 @@ def updateChannelUrlsM3U(channels, template_channels):
                                     
                                     new_url = f"{base_url}{url_suffix}"
                                     
-                                    f_m3u.write(f"#EXTINF:-1 tvg-id=\"\" tvg-name=\"{channel_name}\" tvg-logo=\"https://gitee.com/yuanzl77/TVBox-logo/raw/main/png/{channel_name}.png\" group-title=\"{category}\",{channel_name}\n")
+                                    f_m3u.write(f"#EXTINF:-1 tvg-id=\"{line_number}\" tvg-name=\"{channel_name}\" tvg-logo=\"https://gitee.com/yuanzl77/TVBox-logo/raw/main/png/{channel_name}.png\" group-title=\"{category}\",{channel_name}\n")
                                     f_m3u.write(new_url + "\n")
                                     f_txt.write(f"{channel_name},{new_url}\n")
                                     written_urls.add(url)
