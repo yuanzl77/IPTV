0bc2ae4d3d221ad2d2df5ad721b0e58ecf45981c
圆l <158799657+yuanzl77@users.noreply.github.com>
2024-07-19 00:25:11 +0800

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,8 @@ source_urls = [
     "https://raw.githubusercontent.com/Fairy8o/IPTV/main/PDX-V6.txt",
     "https://live.zhoujie218.top/tv/iptv6.txt",
     "https://tv.youdu.fan:666/live/",
-    "https://fm1077.serv00.net/litv.txt",
+    "http://ww.weidonglong.com/dsj.txt"
+    "http://xhztv.top/zbc.txt",
     "https://raw.githubusercontent.com/mlvjfchen/TV/main/iptv_list.txt",
     "https://raw.githubusercontent.com/qingwen07/awesome-iptv/main/tvbox_live_all.txt",
     "https://v.nxog.top/m/tv/1/",
