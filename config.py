cffbd89f93cd72003131462a941f77131235e62b
leiyou-li <155368804+leiyou-li@users.noreply.github.com>
2024-11-12 12:43:33 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ announcements = [
     {
         "channel": "公告",
         "entries": [
-            {"name": "更新日期", "url": "https://gitlab.com/lr77/IPTV/-/raw/main/%E4%B8%BB%E8%A7%92.mp4", "logo": "http://175.178.251.183:6689/LR.jpg", "time": "2023-10-01 12:00"},
+            {"name": "更新日期"},
             {"name": None, "url": "https://gitlab.com/lr77/IPTV/-/raw/main/%E8%B5%B7%E9%A3%8E%E4%BA%86.mp4", "logo": "http://175.178.251.183:6689/LR.jpg"}
         ]
     }
