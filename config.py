914fd5b7d6eccc3c7ab50a31db5562c04a1cc344
圆l <158799657+yuanzl77@users.noreply.github.com>
2026-08-09 10:56:27 +0800

    Update source URLs in config.py
    Removed multiple source URLs and added a new URL.

--- config.py
+++ config.py
@@ -1,28 +1,7 @@
 ip_version_priority = "ipv6"
 
 source_urls = [
-    "http://175.178.251.183:6689/aktvlive.txt",
-    "https://live.fanmingming.com/tv/m3u/ipv6.m3u",
-    "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/央视频道.txt",
-    "http://120.79.4.185/new/mdlive.txt",
-    "https://raw.githubusercontent.com/Fairy8o/IPTV/main/PDX-V4.txt",
-    "https://raw.githubusercontent.com/Fairy8o/IPTV/main/PDX-V6.txt",
-    "https://live.zhoujie218.top/tv/iptv6.txt",
-    "https://live.zhoujie218.top/tv/iptv4.txt",
-    "https://www.mytvsuper.xyz/m3u/Live.m3u",
-    "https://tv.youdu.fan:666/live/",
-    "http://ww.weidonglong.com/dsj.txt",
-    "http://xhztv.top/zbc.txt",
-    "https://raw.githubusercontent.com/qingwen07/awesome-iptv/main/tvbox_live_all.txt",
-    "https://raw.githubusercontent.com/Guovin/TV/gd/output/result.txt",
-    "http://home.jundie.top:81/Cat/tv/live.txt",
-    "https://raw.githubusercontent.com/vbskycn/iptv/master/tv/hd.txt",
-    "https://cdn.jsdelivr.net/gh/YueChan/live@main/IPTV.m3u",
-    "https://raw.githubusercontent.com/cymz6/AutoIPTV-Hotel/main/lives.txt",
-    "https://raw.githubusercontent.com/PizazzGY/TVBox_warehouse/main/live.txt",
-    "https://fm1077.serv00.net/SmartTV.m3u",
-    "https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt",
-    "https://raw.githubusercontent.com/kimwang1978/collect-tv-txt/main/merged_output.txt"
+    "http://45.192.97.170:6001/txt"
 ]
 
 url_blacklist = [
